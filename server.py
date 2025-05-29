880291e70c219bcb6c737de8f5df1f4f75ddbe60
glen <glen84_dr@hotmail.com>
2025-05-29 12:29:52 +0200

    invalid given values filter

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
     with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
 
 
 def loadCompetitions():
     with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
 
 
 app = Flask(__name__)
@@ -20,46 +20,62 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    club = [club for club in clubs if club['email'] == request.form[
+        'email']][0]
+    return render_template('welcome.html',
+                           club=club,
+                           competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
+def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template('booking.html',
+                               club=foundClub,
+                               competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html',
+                               club=club,
+                               competitions=competitions)
+
 
 def refacto_booking_places(places_required, club, competition):
     """
     This function is a placeholder for refactoring the booking logic.
     It should handle the logic of booking places in a more structured way.
     """
-    if places_required <= 0:
-        return "Invalid number of places requested."
+    if places_required > int(club.get('points')):
+        return 'Sorry, you do not have enough points to book this competition'
     elif places_required > int(competition.get('numberOfPlaces')):
         return 'Sorry, not enough places available'
-    elif places_required > int(club.get('points')):
-        return 'Sorry, you do not have enough points to book this competition'
+    elif places_required < 1 or not places_required.is_integer():
+        print("________________ ", places_required)
+        return "Invalid number of places given."
     else:
         return None
 
 
-
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+    competition = [c for c in competitions if c['name'] == request.form[
+        'competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
+    if not request.form['places'].isdigit():
+        return render_template('booking.html',
+                               club=club,
+                               competition=competition,
+                               message="Invalid number of places given.")
     placesRequired = int(request.form['places'])
 
     problem_booking = refacto_booking_places(placesRequired,
@@ -71,9 +87,9 @@ def purchasePlaces():
                                competition=competition,
                                message=problem_booking)
 
-
     club['points'] = int(club.get('points')) - placesRequired
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    new_val_competition = int(competition['numberOfPlaces'])-placesRequired
+    competition['numberOfPlaces'] = new_val_competition
 
     flash('Great-booking complete!')
     return render_template('welcome.html',
--- test_server.py
+++ test_server.py
@@ -50,12 +50,40 @@ def test_purchasePlaces_not_enough_points():
             'club': 'Club A'}
     with patch('server.competitions',
                [{'name': 'Competition A', 'numberofPlaces': '20'}]), \
-            patch('server.clubs',
-               [{'name': 'Club A', 'points': '9'}]):
+            patch('server.clubs', [{'name': 'Club A', 'points': '9'}]):
         response = client.post('/purchasePlaces', data=data)
-    assert response.status_code == 500
-    assert b'Sorry, you do not have enough points to book this competition' \
+    print("Response Data 2: ", response.data)
+    assert response.status_code == 200
+    assert b"Sorry, you do not have enough points to book this competition" \
         in response.data
+
+
+def test_purchasePlaces_value_error():
+    """
+    Test the purchasePlaces route when the input values are invalid.
+    """
+    client = app.test_client()
+    client.testing = True
+
+    data = {'places': '0',
+            'competition': 'Competition A',
+            'club': 'Club A'}
+    data2, data3 = data.copy(), data.copy()
+    data2['places'] = 'abc'
+    data3['places'] = '0.0'
+    with patch('server.competitions',
+               [{'name': 'Competition A', 'numberOfPlaces': 5}]), \
+            patch('server.clubs', [{'name': 'Club A', 'points': '10'}]):
+        response = client.post('/purchasePlaces', data=data)
+        response2 = client.post('/purchasePlaces', data=data2)
+        response3 = client.post('/purchasePlaces', data=data3)
+
+    assert response.status_code == 200
+    assert b"Invalid number of places given." in response.data
+    assert response2.status_code == 200
+    assert b"Invalid number of places given." in response2.data
+    assert response3.status_code == 200
+    assert b"Invalid number of places given." in response3.data
 
 
 if __name__ == '__main__':
