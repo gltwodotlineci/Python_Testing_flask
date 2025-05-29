2fabb04885892e0fee4101610132e98c0e964d34
glen <glen84_dr@hotmail.com>
2025-05-29 21:24:37 +0200

    implemanting patch

--- test_server.py
+++ test_server.py
@@ -2,7 +2,6 @@ from server import app
 import pytest
 from unittest.mock import patch
 
-
 @pytest.fixture
 def client_and_data():
     """
@@ -17,34 +16,41 @@ def client_and_data():
         yield client, data0
 
 
-def test_purchasePlaces_success(client_and_data):
+@pytest.fixture()
+def patch_data():
+    """
+    Fixture to patch the data for competitions and clubs.
+    """
+    competitions = [{'name': 'Competition A', 'numberOfPlaces': '5'}]
+    clubs = [{'name': 'Club A', 'points': '10'}]
+    with patch(
+        'server.competitions', competitions), patch(
+            'server.clubs', clubs):
+        yield competitions, clubs
+
+
+def test_purchasePlaces_success(client_and_data, patch_data):
     """
     Test the purchasePlaces route for successful booking.
     """
     client, data = client_and_data
     data['places'] = "2"
-    with patch('server.competitions',
-               [{'name': 'Competition A', 'numberOfPlaces': 5}]), \
-            patch('server.clubs', [{'name': 'Club A', 'points': '10'}]):
-        response = client.post('/purchasePlaces', data=data)
+    response = client.post('/purchasePlaces', data=data)
     assert response.status_code == 200
     assert b'Great-booking complete!' in response.data
 
 
-def test_purchasePlaces_not_enough_places(client_and_data):
+def test_purchasePlaces_not_enough_places(client_and_data, patch_data):
     """
     Test the purchasePlaces route when there are not enough places available.
     """
     client, data = client_and_data
-    with patch('server.competitions',
-               [{'name': 'Competition A', 'numberOfPlaces': 5}]), \
-            patch('server.clubs', [{'name': 'Club A', 'points': '10'}]):
-        response = client.post('/purchasePlaces', data=data)
+    response = client.post('/purchasePlaces', data=data)
     assert response.status_code == 200
     assert b'Sorry, not enough places available' in response.data
 
 
-def test_purchasePlaces_not_enough_points(client_and_data):
+def test_purchasePlaces_not_enough_points(client_and_data, patch_data):
     """
     Test the purchasePlaces when the club does not have enough points.
     """
@@ -59,7 +65,7 @@ def test_purchasePlaces_not_enough_points(client_and_data):
         in response.data
 
 
-def test_purchasePlaces_value_error(client_and_data):
+def test_purchasePlaces_value_error(client_and_data, patch_data):
     """
     Test the purchasePlaces route when the input values are invalid.
     """
@@ -68,12 +74,10 @@ def test_purchasePlaces_value_error(client_and_data):
     data['places'] = '0'
     data2['places'] = 'abc'
     data3['places'] = '0.0'
-    with patch('server.competitions',
-               [{'name': 'Competition A', 'numberOfPlaces': 5}]), \
-            patch('server.clubs', [{'name': 'Club A', 'points': '10'}]):
-        response = client.post('/purchasePlaces', data=data)
-        response2 = client.post('/purchasePlaces', data=data2)
-        response3 = client.post('/purchasePlaces', data=data3)
+
+    response = client.post('/purchasePlaces', data=data)
+    response2 = client.post('/purchasePlaces', data=data2)
+    response3 = client.post('/purchasePlaces', data=data3)
 
     assert response.status_code == 200
     assert b"Invalid number of places given." in response.data
