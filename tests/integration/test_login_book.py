a07b3d092b8c406be1cedebeff8f94a8f88a8fff
glen <glen84_dr@hotmail.com>
2025-06-10 11:21:10 +0200

    parametrized login book integration test

--- tests/integration/test_login_book.py
+++ tests/integration/test_login_book.py
@@ -3,24 +3,25 @@ from tests.unit.test_login_logout import club_email, patch_dt_club, \
 from bs4 import BeautifulSoup
 import pytest
 
-msg1 = "Great-booking complete!"
-msg2 = "You can not book more than 12 places for this competition"
-msg3 = "Sorry, you do not have enough points to book this competition"
-msg4 = "Sorry, not enough places available"
-msg5 = "The competition you chose is not available anymore"
+
+msg_d = {'1': "Great-booking complete!",
+         '2': "You can not book more than 12 places for this competition",
+         '3': "Sorry, you do not have enough points to book this competition",
+         '4': "Sorry, not enough places available",
+         '5': "The competition you chose is not available anymore"}
 
 @pytest.mark.parametrize("club_name, clb_pts, clb_email, \
                          comp_name, date, comp_plcs, places_req, msg",
                          [("clb1", "20", "exmp@oc.fr", "CompA",
-                           "2020-03-27 10:00:00", "15", "2", msg1),
+                           "2020-03-27 10:00:00", "15", "2", msg_d['1']),
                           ("clb1", "20", "exmp@oc.fr", "CompA",
-                           "2020-03-27 10:00:00", "15", "11", msg2),
+                           "2020-03-27 10:00:00", "15", "11", msg_d['2']),
                           ("clb1", "15", "exmp@oc.fr", "CompA",
-                           "2020-03-27 10:00:00", "15", "16", msg3),
+                           "2020-03-27 10:00:00", "15", "16", msg_d['3']),
                           ("clb1", "6", "exmp@oc.fr", "CompA",
-                           "2020-03-27 10:00:00", "2", "3", msg4),
+                           "2020-03-27 10:00:00", "2", "3", msg_d['4']),
                           ("clb1", "6", "exmp@oc.fr", "CompA",
-                           "2020-03-27 10:00:00", "6", "6", msg5)
+                           "2020-03-27 10:00:00", "6", "6", msg_d['5'])
                            
                            ])
 def test_login_book_place(club_email, patch_dt_club, mock_current_user,
@@ -49,11 +50,12 @@ def test_login_book_place(club_email, patch_dt_club, mock_current_user,
         resp_booking = client.get(book_url)
     assert resp_booking.status_code == 200
     # Checking get in booking page
-    assert f"Places available: {comp_plcs}" in resp_booking.data.decode('utf-8')
+    assert f"Places available: {comp_plcs}" in \
+        resp_booking.data.decode('utf-8')
 
     # booking:
     # Make an exception when competition has no places
-    if msg == msg5:
+    if msg == msg_d['5']:
         comp_plcs = "0"
     with patch_dt_club(club_name, clb_pts, clb_email, comp_name, date,
                        comp_plcs) as (clubs, competitions):
@@ -71,4 +73,5 @@ def test_login_book_place(club_email, patch_dt_club, mock_current_user,
         assert f"Points available: {clb.get('points')}" in resp_pourchase
         assert comp_name in resp_pourchase
         assert f"Date: {date}" in resp_pourchase
-        assert f"Number of Places: {compet['numberOfPlaces']}" in resp_pourchase
+        assert f"Number of Places: {compet['numberOfPlaces']}" \
+            in resp_pourchase
